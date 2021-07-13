1d4688eec279ede01da0c82bca62f383767ba93f
dracozhan <dracozhan@tencent.com>
2021-07-13 13:24:22 +0800

    pa4 bezier with aa

---
+++ pa4/Assignment4/README.md
@@ -0,0 +1,22 @@
+# GAMES101 homework4
+
+## 完成
+- De Casteljau 算法
+- Bonus:  Bézier 曲线的反走样
+
+## 说明
+
+### 反走样
+- 使用双线性插值法实现
+## Result
+
+### no AA
+![noAA](./images/noAA.png)
+
+![noAABlend](./images/noAA_Blend.png)
+
+### AA
+![AA](./images/AA.png)
+
+![AABlend](./images/AA_Blend.png)
+
--- pa4/Assignment4/main.cpp
+++ pa4/Assignment4/main.cpp
@@ -1,8 +1,65 @@
 #include <chrono>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <algorithm>
+#include <unordered_map>
 
 std::vector<cv::Point2f> control_points;
+
+/// AA begin
+
+float color(float y, float x, cv::Mat& window)
+{
+	return window.at<cv::Vec3b>(y, x)[1];
+}
+
+float lerp(float x, float v0, float v1)
+{
+	return v0 + x * (v1 - v0);
+}
+
+int AA(const cv::Point2f& point, cv::Mat& window)
+{
+	// horizontal
+	int R = point.y;
+	float u = R + 0.5f;
+
+	int C = point.x;
+	float v = C + 0.5f;
+
+	int R0 = std::clamp(R - 1, 0, 699);
+	int R2 = std::clamp(R + 1, 0, 699);
+
+	int u0 = R;
+	int u1 = R2;
+	if (point.y <= u)
+	{
+		u0 = R0;
+		u1 = R;
+	}
+	float s = std::abs(point.y - R);
+
+	// vertical
+	int C0 = std::clamp(C - 1, 0, 699);
+	int C2 = std::clamp(C + 1, 0, 699);
+
+	int v0 = C;
+	int v1 = C2;
+	if (point.x <= v)
+	{
+		v0 = C0;
+		v1 = C;
+	}
+	float t = std::abs(point.x - C);
+
+	// lerp
+	float a0 = lerp(s, color(u0, v0, window), color(u0, v1, window));
+	float a1 = lerp(s, color(u1, v0, window), color(u1, v1, window));
+
+	return lerp(t, a0, a1);
+}
+
+/// AA end
 
 void mouse_handler(int event, int x, int y, int flags, void *userdata) 
 {
@@ -33,15 +90,47 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
 cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
+    if (control_points.size() == 1)
+    {
+        return control_points[0];
+    }
+    
+    std::vector<cv::Point2f> new_points;
+    for (int iP=0; iP<control_points.size()-1; ++iP)
+    {
+        const auto& p1 = control_points[iP];
+        const auto& p2 = control_points[iP+1];
+        cv::Point2f np(p1.x*t+p2.x*(1-t), p1.y*t+p2.y*(1-t));
+        new_points.push_back(np);
+    }
 
+    return recursive_bezier(new_points,  t);
 }
 
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
+    std::vector<cv::Point2f> vPoints;
+    for (float t=0.0; t<=1.0; t+=0.001)
+    {
+        auto point = recursive_bezier(control_points, t);
+        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+        vPoints.push_back(point);
+    }
 
+    // AA
+    std::vector<int> vColor;
+    for (auto p : vPoints)
+    {
+        vColor.push_back( AA(p, window) );
+    }
+
+    for (int i=0; i<vColor.size(); ++i)
+    {
+        const auto p = vPoints[i];
+        window.at <cv::Vec3b>(p.y, p.x)[1] = vColor[i];
+    }
 }
 
 int main() 
@@ -63,7 +152,7 @@ int main()
         if (control_points.size() == 4) 
         {
             naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
